00aa7a5adcb9c6337410ac911d3a55dddd24fbda
Jose Flores <alfredo_flores123@yahoo.com>
2018-08-22 23:16:28 +0000

    added a next lesson button and code that lets the user move to the next section and the first lesson of that section

--- config/routes.rb
+++ config/routes.rb
@@ -2,24 +2,26 @@ Rails.application.routes.draw do
   devise_for :users
   resource :dashboard, only: [:show]
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-root 'static_pages#index'
-resources :courses, only: [:index, :show] do
-  resources :enrollments, only: :create
-end
-resources :lessons, only: [:show]
-namespace :instructor do
-  resources :sections, only: [:update]
-  resources :lessons, only: [:update]
-  resources :sections, only: [] do
-    
+  root 'static_pages#index'
+  resources :courses, only: [:index, :show] do
+    resources :enrollments, only: :create
   end
-  resources :courses, only: [:new, :create, :show] do
-    resources :sections, only: [:create] 
+  resources :lessons, only: [:show]
+    namespace :instructor do
+      
+      resources :sections, only: [:update] do
+        resources :lessons, only: :create
+      end
+   
+      
+      resources :lessons, only: [:update]
+      resources :sections, only: [] do
+    end
+    resources :courses, only: [:new, :create, :show] do
+      resources :sections, only: [:create]
     end
   end 
-end
-
-
+end   
 
 
 
